0738586b0ed4c2fb76f10df3e2c47eec2f3da44a
rotem <rotem.weissman@mail.huji.ac.il>
2021-05-03 11:17:34 +0300

    this is test 1

--- wave_editor.py
+++ wave_editor.py
@@ -1,2 +1,10 @@
+import wave_helper
+
+
+EDIT_MENU = {"REVERS": "1", "MINUS": "2", "FAST FORWARD": "3", "SLOW_DOWN": "4",
+             "VOLUME_UP": "5", "VOLUME_DOWN": "6", "LOW_PASS_FILTER": "7", "FINISH_EDIT": "8"}
+START_MENU = {"EDIT_AUDIO": "1", "CREATE_AUDIO": "2", "CLOSE": "3"}
+
+
 if __name__ == '__main__':
     pass
