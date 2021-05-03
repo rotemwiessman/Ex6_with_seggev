34d7b4a0f765dd54c8f35e843585dcf6296d533a
rotemwiessman <83572309+rotemwiessman@users.noreply.github.com>
2021-05-03 11:21:05 +0300

    Add files via upload

--- wave_editor.py
+++ wave_editor.py
@@ -5,6 +5,8 @@ EDIT_MENU = {"REVERS": "1", "MINUS": "2", "FAST FORWARD": "3", "SLOW_DOWN": "4",
              "VOLUME_UP": "5", "VOLUME_DOWN": "6", "LOW_PASS_FILTER": "7", "FINISH_EDIT": "8"}
 START_MENU = {"EDIT_AUDIO": "1", "CREATE_AUDIO": "2", "CLOSE": "3"}
 
+def minus_audio():
+    pass
 
 if __name__ == '__main__':
     pass
